75ef622ecd379e5f7b2dd9ae17b6dba8e1e965d8
shreyash dhamane <shreyashb.dhamane0@gmail.com>
2023-07-02 18:16:37 +0530

    .

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const express = require("express");
 const WebSocket = require("ws");
 
 const app = express();
+
+app.get("/", function (req, res) {
+  res.status(200).send("Hello World!");
+});
+
+app.post("/payment_success", function (req, res) {
+  console.log("Payment success");
+  res.status(200).send("Payment success");
+});
+
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
